cb735835f0b79879f991912a08c85bad2c0bf100
dhanush <dhanushshetty9741@gmail.com>
2024-12-14 15:55:07 +0530

    appointment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';  // Keep only one import of React
 import ReactDOM from 'react-dom/client';
 
 
-
 import App from './App';  // Updated path to App.js
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
